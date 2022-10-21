b62e24d61f17c15e49fba525c29c3c4edfb4430c
unknown <gabypelle12@gmail.com>
2022-10-21 14:55:26 -0300

    main

---
+++ src/components/TableCoins.js
@@ -0,0 +1,36 @@
+import React from "react";
+import CoinRows from "./CoinRow";
+import './TableCoins.css'
+
+const titles = ['#', 'Coin', 'Price', 'Price Change', '24h Volume']
+
+const TableCoins = ({coins, search}) => {
+
+    const filteredCoins = coins.filter(
+        (coin) =>
+            coin.name.toLowerCase().includes(search.toLowerCase())|
+            coin.symbol.toLowerCase().includes(search.toLowerCase()))
+
+    return (
+        <table className="table table-dark mt-4 table-hover">
+            <thead>
+                <tr>
+                    {
+                    titles.map((title,index) =>(
+                        <td key={index}>
+                        {title}
+                        </td>
+                    ))
+                    }
+                </tr>
+            </thead>
+            <tbody>
+                {filteredCoins.map((coin, index)=> (
+                    <CoinRows coin={coin} key={index} index={index + 1}/>
+                ))}
+            </tbody>
+        </table>
+    )
+}
+
+export default TableCoins
